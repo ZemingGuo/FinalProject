a8d3d11ae8ac1816b1f5d77eea5bfe130b0c374c
JerryGuo <zemingg2@illinois.edu>
2019-12-01 00:05:51 -0600

    initialization

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //balabalabala
+        //今天是十二月一日
     }
 }
