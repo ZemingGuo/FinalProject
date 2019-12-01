2e4cf992d71e7448898e1ccaa4dff70d99f6c29d
CatherineCheng0626 <wanning3@illinois.edu>
2019-12-01 04:18:27 -0600

    First page completed

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //balabalabala
-        //今天是十二月一日
+        Button startGame = findViewById(R.id.startGame);
+        Intent intent = new Intent(this, GameActivity.class);
+        startGame.setOnClickListener(unused -> startActivity(intent));
     }
 }
