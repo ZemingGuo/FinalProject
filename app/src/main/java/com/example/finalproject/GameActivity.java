e7426b316612e8d203b27021616ad3c4e314edf4
JerryGuo <zemingg2@illinois.edu>
2019-12-01 18:46:22 -0600

    第一二页

--- app/src/main/java/com/example/finalproject/GameActivity.java
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -2,7 +2,14 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +17,12 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        List<String> conversation = new ArrayList<String>();
+
+        ImageButton next = findViewById(R.id.next);
+        TextView conv = findViewById(R.id.conversation);
+        next.setOnClickListener(unused -> conv.setText("其实我一直都很喜欢你！"));
+
+
     }
 }
